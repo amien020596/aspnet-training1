79c471ed295416183f8dacb3d944977956d50026
Amien Kurniawan <akurniawan@tesla.com>
2021-06-18 09:24:01 +0700

    update unfinish solution 2

--- WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/Controllers/MoviesController.cs
@@ -10,7 +10,20 @@ namespace WebApplication1.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movie
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id = 1, Name="Shrek"},
+                new Movie{Id = 2, Name="Wall-e"}
+            };
+        }
+        // GET: Movie/Random
         public ActionResult Random()
         {
             var movies = new Movie() { Name = "Shrek!" };
